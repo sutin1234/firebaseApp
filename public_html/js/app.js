06831cdfe3d5b7dc81adf238b012c8db616e6d69
sutin <tony.sutin1234@gmail.com>
2015-09-18 11:42:29 +0700

    CRUD in Firebase
    การทำระบบ CRUD ใน firebase

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,13 +1,52 @@
 var app = angular.module('firebaseApp',['firebase']);
 
 var ref = new Firebase("https://kohanthailand.firebaseio.com/chatroom/tb_user");
+app.service('ServiceFirebase',function($firebaseArray){
+	var dataRef = $firebaseArray(ref);
+	this.isLoaded = function(){
+		return dataRef;
+	}
+});
+app.controller('firebaseCtrl',function($scope,$firebaseArray,ServiceFirebase){
+	$scope.dataArray = ServiceFirebase.isLoaded();
+	$scope.isEdit = false;
+	$scope.isAdd = false;
 
-app.controller('firebaseCtrl',function($scope,$firebaseArray){
-	$scope.dataArray = $firebaseArray(ref);
-
+	// AddUser
 	$scope.AddUser = function(User){
+		$scope.User = [];
+		$scope.isAdd = true;
+	}
+
+	// onAdd
+	$scope.onAdd = function(User){
 		$scope.dataArray.$add(User);
 		$scope.User = [];
+		$scope.isAdd = false;
+	}
+
+	//onDele
+	$scope.onDele = function(User){
+		$scope.dataArray.$remove(User);
+	}
+
+	//onEdit
+	$scope.onEdit = function(User){
+			$scope.User = User;
+			$scope.isEdit = true;
+	}
+
+	//onUpdate
+	$scope.onUpdate = function(User){
+		$scope.dataArray.$save(User);
+		$scope.User = [];
+		$scope.isEdit = false;
+	}
+
+	//onCancelForm
+	$scope.onCancelForm = function(){
+		$scope.isEdit = false;
+		$scope.isAdd = false;
 	}
 
 });
